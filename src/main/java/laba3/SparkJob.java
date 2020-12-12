962b2fa10837ee7654b26f7a9a7207c4b6be6734
snailfighting <vip.milael@gmail.com>
2020-12-12 12:59:22 +0300

    Scripted auto-commit on change (2020-12-12 12:59:22) by gitwatch.sh

--- src/main/java/laba3/SparkJob.java
+++ src/main/java/laba3/SparkJob.java
@@ -1,10 +1,18 @@
 package laba3;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkJob {
+    private static final String NAMEDILIMETR = "\",";
+    private static final String DELAYDElIMETR = ",";
+    private static final int NAMEAIRPORT = 1;
+    private static final int ARRDELAY = 17;
+    private static final String NULLSRING = "";
+    private static final float ZERO = 0.0f;
+
     public static void main(){
         SparkConf conf = new SparkConf().setAppName("lab5");
         JavaSparkContext sc = new JavaSparkContext(conf);
@@ -12,6 +20,7 @@ public class SparkJob {
         JavaRDD<String> AirportNames = sc.textFile("L_AIRPORT_ID.csv");
         JavaRDD<String> AirportDelays = sc.textFile("664600583_T_ONTIME_sample.csv");
 
-        
+        JavaPairRDD<Integer, Integer>, 
+
     }
 }
