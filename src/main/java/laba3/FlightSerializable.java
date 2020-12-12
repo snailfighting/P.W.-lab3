3fc105b23f2bc6bb62842377c58e3d868127961b
snailfighting <vip.milael@gmail.com>
2020-12-12 13:56:41 +0300

    Scripted auto-commit on change (2020-12-12 13:56:41) by gitwatch.sh

--- src/main/java/laba3/FlightSerializable.java
+++ src/main/java/laba3/FlightSerializable.java
@@ -6,5 +6,15 @@ public class FlightSerializable implements Serializable {
     public FlightSerializable(){}
 
     private float arrDelay;
-    private float
+    private float cancelled;
+
+    private int airportID;
+    private int originalAirportID;
+
+    public FlightSerializable(int airportID, int originalAirportID, float arrDelay, float cancelled){
+        this.airportID = airportID;
+        this.arrDelay = arrDelay;
+        this.cancelled = cancelled;
+        this.originalAirportID = originalAirportID;
+    }
 }
