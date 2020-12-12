68bfc4b883106c30f86a0af850654e074f375c6f
snailfighting <vip.milael@gmail.com>
2020-12-12 12:03:48 +0300

    Scripted auto-commit on change (2020-12-12 12:03:48) by gitwatch.sh

---
+++ src/main/java/laba3/SparkJob.java
@@ -0,0 +1,10 @@
+package laba3;
+
+import org.apache.spark.SparkConf;
+
+public class SparkJob {
+    public static void main(){
+        SparkConf conf = new SparkConf().setAppName("lab5");
+        
+    }
+}
