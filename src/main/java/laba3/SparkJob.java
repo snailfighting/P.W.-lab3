06d2b954ea933ca683b73daab0815ee3e59f6763
snailfighting <vip.milael@gmail.com>
2020-12-12 13:51:40 +0300

    Scripted auto-commit on change (2020-12-12 13:51:40) by gitwatch.sh

--- src/main/java/laba3/SparkJob.java
+++ src/main/java/laba3/SparkJob.java
@@ -13,9 +13,12 @@ public class SparkJob {
     private static final int ARRDELAY = 17;
     private static final String NULLSRING = "";
     private static final float ZERO = 0.0f;
+
     private static final int DESTAIRPORTIDFORNAMES= 0;
-    private static final int ORIGINALAIRPORTID = 11;
+    private static final int DESTAIRPORTIDFORDELAYS = 14;
     
+    private static final int ORIGINALAIRPORTID = 11;
+
 
     public static void main(){
         SparkConf conf = new SparkConf().setAppName("lab5");
