dd10abde3d04b57e66e595d5464786bb45e0016e
snailfighting <vip.milael@gmail.com>
2020-12-13 18:14:13 +0300

    Scripted auto-commit on change (2020-12-13 18:14:13) by gitwatch.sh

--- src/main/java/laba3/FlightSerializablCount.java
+++ src/main/java/laba3/FlightSerializablCount.java
@@ -3,8 +3,32 @@ package laba3;
 import java.io.Serializable;
 
 public class FlightSerializablCount implements Serializable {
-    public FlightSerializablCount() {
+    public FlightSerializablCount() {}
+    private float maxArrDelay;
+    private int delayCount;
+    private int flightCount;
+    private int cancelledCount;
 
+    public FlightSerializablCount(int cancelledCount, int delayCount, int flightCount, float maxArrDelay){
+        this.cancelledCount = cancelledCount;
+        this.delayCount = delayCount;
+        this.flightCount = flightCount;
+        this.maxArrDelay = maxArrDelay;
+    }
+    public float getMaxArrDelay(){
+        return maxArrDelay;
+    }
+    public int getDelayCount(){
+        return delayCount;
+    }
+    public int getFlightCount(){
+        return flightCount;
+    }
+    public int getCancelledCount(){
+        return cancelledCount;
+    }
+
+    public static FlightSerializablCount addValue(FlightSerializablCount a, float maxArrDelay, bollean deayed, boolean cencelled){
         
     }
 }
